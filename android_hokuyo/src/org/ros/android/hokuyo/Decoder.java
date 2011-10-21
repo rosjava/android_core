449411ef0168003522f326d1f524bbb97483ad3b
Lorenz Moesenlechner <moesenle@google.com>
2011-10-21 14:06:59 +0200

    Made decoder more generic
     * It can decode block sizes different from 3 now
    
     * Added static method decodeValue to just decode one value, not a complete
       buffer.

--- android_hokuyo/src/org/ros/android/hokuyo/Decoder.java
+++ android_hokuyo/src/org/ros/android/hokuyo/Decoder.java
@@ -24,24 +24,21 @@ import com.google.common.collect.Lists;
 
 public class Decoder {
 
-  @VisibleForTesting
-  static int decode3Letter(String buffer) {
-    Preconditions.checkArgument(buffer.length() == 3);
-    int high = (buffer.charAt(0) - 0x30) << 12;
-    int mid = (buffer.charAt(1) - 0x30) << 6;
-    int low = (buffer.charAt(2) - 0x30);
-    return high + mid + low;
+  static int decodeValue(String buffer, int blockSize) {
+    Preconditions.checkArgument(buffer.length() == blockSize);
+    int result = 0;
+    for(int i=0; i<blockSize; i++) {
+      result |= (buffer.charAt(blockSize - i - 1) - 0x30) << i * 6;
+    }
+    return result;
   }
 
   public static List<Float> decode(String buffer, int blockSize) {
-    Preconditions.checkArgument(blockSize == 3);
     Preconditions.checkArgument(buffer.length() % blockSize == 0);
     List<Float> data = Lists.newArrayList();
     for (int i = 0; i < buffer.length(); i += blockSize) {
-      if (blockSize == 3) {
-        // sensor_msgs/LaserScan uses floats for ranges.
-        data.add((float) decode3Letter(buffer.substring(i, i + 3)));
-      }
+      // sensor_msgs/LaserScan uses floats for ranges.
+      data.add((float) decodeValue(buffer.substring(i, i + 3), blockSize));
     }
     return data;
   }
