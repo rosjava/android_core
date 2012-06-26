793493f147bbe582f01adde309c586b18c4ce09e
Damon Kohler <damonkohler@google.com>
2012-06-26 17:02:47 +0200

    Add benchmark app.

---
+++ android_benchmarks/src/org/ros/android/android_benchmarks/MainActivity.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2012 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.ros.android.android_benchmarks;
+
+import android.os.Bundle;
+import org.ros.android.MessageCallable;
+import org.ros.android.RosActivity;
+import org.ros.android.view.RosTextView;
+import org.ros.node.NodeConfiguration;
+import org.ros.node.NodeMainExecutor;
+import org.ros.rosjava_benchmarks.PubsubBenchmark;
+
+/**
+ * @author damonkohler@google.com (Damon Kohler)
+ */
+public class MainActivity extends RosActivity {
+
+  private RosTextView<std_msgs.String> rosTextView;
+
+  public MainActivity() {
+    super("Benchmarks", "Benchmarks");
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    rosTextView = (RosTextView<std_msgs.String>) findViewById(R.id.text);
+    rosTextView.setTopicName("status");
+    rosTextView.setMessageType(std_msgs.String._TYPE);
+    rosTextView.setMessageToStringCallable(new MessageCallable<String, std_msgs.String>() {
+      @Override
+      public String call(std_msgs.String message) {
+        return message.getData();
+      }
+    });
+  }
+
+  @Override
+  protected void init(NodeMainExecutor nodeMainExecutor) {
+    NodeConfiguration nodeConfiguration = NodeConfiguration.newPrivate();
+    nodeConfiguration.setMasterUri(getMasterUri());
+    nodeMainExecutor.execute(rosTextView, nodeConfiguration);
+    nodeMainExecutor.execute(new PubsubBenchmark(), nodeConfiguration);
+  }
+}
