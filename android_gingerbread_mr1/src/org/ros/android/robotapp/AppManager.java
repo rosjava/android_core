4b30506239e53a1dbe15a18a0d6be60c100bca2b
KazutoMurase <kattyokaccho3616@gmail.com>
2013-02-01 11:27:24 -0800

    update copyright

--- android_gingerbread_mr1/src/org/ros/android/robotapp/AppManager.java
+++ android_gingerbread_mr1/src/org/ros/android/robotapp/AppManager.java
@@ -1,6 +1,24 @@
+/*
+ * Copyright (C) 2013 OSRF.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 package org.ros.android.robotapp;
 
-import org.ros.exception.RemoteException;
+
+import android.util.Log;
+
 import org.ros.exception.RosRuntimeException;
 import org.ros.exception.ServiceNotFoundException;
 import org.ros.namespace.GraphName;
@@ -9,7 +27,6 @@ import org.ros.node.ConnectedNode;
 import org.ros.node.service.ServiceClient;
 import org.ros.node.service.ServiceResponseListener;
 
-import android.util.Log;
 import app_manager.ListApps;
 import app_manager.ListAppsRequest;
 import app_manager.ListAppsResponse;
@@ -20,97 +37,104 @@ import app_manager.StopApp;
 import app_manager.StopAppRequest;
 import app_manager.StopAppResponse;
 
-public class AppManager extends AbstractNodeMain{
+/**
+ * @author murase@jsk.imi.i.u-tokyo.ac.jp (Kazuto Murase)
+ */
+public class AppManager extends AbstractNodeMain {
 
-    static public final String PACKAGE = "org.ros.android";
+	static public final String PACKAGE = "org.ros.android";
 	private String appName;
 	private ServiceResponseListener<StartAppResponse> startServiceResponseListener;
 	private ServiceResponseListener<StopAppResponse> stopServiceResponseListener;
 	private ServiceResponseListener<ListAppsResponse> listServiceResponseListener;
-	
+
 	private ConnectedNode connectedNode;
 	private String function;
-	
-	public AppManager(final String appName){
+
+	public AppManager(final String appName) {
 		this.appName = appName;
 	}
-	
-	public void setFunction(String function){
+
+	public void setFunction(String function) {
 		this.function = function;
 	}
-	
-	public void setStartService(ServiceResponseListener<StartAppResponse> startServiceResponseListener){
+
+	public void setStartService(
+			ServiceResponseListener<StartAppResponse> startServiceResponseListener) {
 		this.startServiceResponseListener = startServiceResponseListener;
 	}
-	
-	public void setStopService(ServiceResponseListener<StopAppResponse> stopServiceResponseListener){
+
+	public void setStopService(
+			ServiceResponseListener<StopAppResponse> stopServiceResponseListener) {
 		this.stopServiceResponseListener = stopServiceResponseListener;
 	}
-	
-	public void setListService(ServiceResponseListener<ListAppsResponse> listServiceResponseListener){
+
+	public void setListService(
+			ServiceResponseListener<ListAppsResponse> listServiceResponseListener) {
 		this.listServiceResponseListener = listServiceResponseListener;
 	}
-	
-    public void startApp() {
-    	
-        ServiceClient<StartAppRequest, StartAppResponse> startAppClient;
-    	try{
-                Log.i("RosAndroid", "Start app service client created");
-                startAppClient = connectedNode.newServiceClient("/turtlebot/start_app", StartApp._TYPE);
-    	} catch(ServiceNotFoundException e) {
-    		throw new RosRuntimeException(e);
-    	}
-	    final StartAppRequest request = startAppClient.newMessage();
-	    request.setName(appName);
-	    startAppClient.call(request,startServiceResponseListener);
-	    Log.i("RosAndroid", "Done call");
-    }
-    
-   public void stopApp() {
-    	
-        ServiceClient<StopAppRequest, StopAppResponse> stopAppClient;
-    	try{
-                Log.i("RosAndroid", "Stop app service client created");
-                stopAppClient = connectedNode.newServiceClient("/turtlebot/stop_app", StopApp._TYPE);
-    	} catch(ServiceNotFoundException e) {
-    		throw new RosRuntimeException(e);
-    	}
-	    final StopAppRequest request = stopAppClient.newMessage();
-	    request.setName(appName);
-	    stopAppClient.call(request,stopServiceResponseListener);
-	    Log.i("RosAndroid", "Done call");
-    }
-   
-   public void listApps() {
-	   
-	   ServiceClient<ListAppsRequest, ListAppsResponse> listAppsClient;
-	   try{
-           Log.i("RosAndroid", "List app service client created");
-           listAppsClient = connectedNode.newServiceClient("/turtlebot/list_apps", ListApps._TYPE);
-	   } catch(ServiceNotFoundException e){
-		   throw new RosRuntimeException(e);
-	   }
-	   final ListAppsRequest request = listAppsClient.newMessage();
-	   listAppsClient.call(request, listServiceResponseListener);
-	   Log.i("RosAndroid","Done call");
-   }
+
+	public void startApp() {
+
+		ServiceClient<StartAppRequest, StartAppResponse> startAppClient;
+		try {
+			Log.i("RosAndroid", "Start app service client created");
+			startAppClient = connectedNode.newServiceClient(
+					"/turtlebot/start_app", StartApp._TYPE);
+		} catch (ServiceNotFoundException e) {
+			throw new RosRuntimeException(e);
+		}
+		final StartAppRequest request = startAppClient.newMessage();
+		request.setName(appName);
+		startAppClient.call(request, startServiceResponseListener);
+		Log.i("RosAndroid", "Done call");
+	}
+
+	public void stopApp() {
+
+		ServiceClient<StopAppRequest, StopAppResponse> stopAppClient;
+		try {
+			Log.i("RosAndroid", "Stop app service client created");
+			stopAppClient = connectedNode.newServiceClient(
+					"/turtlebot/stop_app", StopApp._TYPE);
+		} catch (ServiceNotFoundException e) {
+			throw new RosRuntimeException(e);
+		}
+		final StopAppRequest request = stopAppClient.newMessage();
+		request.setName(appName);
+		stopAppClient.call(request, stopServiceResponseListener);
+		Log.i("RosAndroid", "Done call");
+	}
+
+	public void listApps() {
+
+		ServiceClient<ListAppsRequest, ListAppsResponse> listAppsClient;
+		try {
+			Log.i("RosAndroid", "List app service client created");
+			listAppsClient = connectedNode.newServiceClient(
+					"/turtlebot/list_apps", ListApps._TYPE);
+		} catch (ServiceNotFoundException e) {
+			throw new RosRuntimeException(e);
+		}
+		final ListAppsRequest request = listAppsClient.newMessage();
+		listAppsClient.call(request, listServiceResponseListener);
+		Log.i("RosAndroid", "Done call");
+	}
 
 	@Override
 	public GraphName getDefaultNodeName() {
-				return null;
+		return null;
 	}
-	
+
 	@Override
-	public void onStart(final ConnectedNode connectedNode){
+	public void onStart(final ConnectedNode connectedNode) {
 		this.connectedNode = connectedNode;
-		if(function.equals("start")){
+		if (function.equals("start")) {
 			startApp();
-		}
-		else if(function.equals("stop")){
+		} else if (function.equals("stop")) {
 			stopApp();
-		}
-		else if(function.equals("list")){
+		} else if (function.equals("list")) {
 			listApps();
 		}
-	}	
+	}
 }
--- android_gingerbread_mr1/src/org/ros/android/robotapp/RosAppActivity.java
+++ android_gingerbread_mr1/src/org/ros/android/robotapp/RosAppActivity.java
@@ -1,5 +1,20 @@
-package org.ros.android.robotapp;
+/*
+ * Copyright (C) 2013 OSRF.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
 
+package org.ros.android.robotapp;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -13,8 +28,6 @@ import android.view.KeyEvent;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
-import app_manager.StartAppResponse;
-import app_manager.StopAppResponse;
 
 import org.ros.address.InetAddressFactory;
 import org.ros.android.RosActivity;
@@ -24,189 +37,198 @@ import org.ros.node.NodeConfiguration;
 import org.ros.node.NodeMainExecutor;
 import org.ros.node.service.ServiceResponseListener;
 
-public abstract class RosAppActivity extends RosActivity
-{
+import app_manager.StartAppResponse;
+import app_manager.StopAppResponse;
 
-    private String robotAppName = null, defaultAppName = null;
-    private boolean startApplication = true;
-    private int dashboardResourceId = 0;
-    private int mainWindowId = 0;
-    private Dashboard dashboard = null;
-    private NodeConfiguration nodeConfiguration;
+/**
+ * @author murase@jsk.imi.i.u-tokyo.ac.jp (Kazuto Murase)
+ */
+public abstract class RosAppActivity extends RosActivity {
+
+	private String robotAppName = null, defaultAppName = null;
+	private boolean startApplication = true;
+	private int dashboardResourceId = 0;
+	private int mainWindowId = 0;
+	private Dashboard dashboard = null;
+	private NodeConfiguration nodeConfiguration;
 	private NodeMainExecutor nodeMainExecutor;
 	private boolean fromAppChooser = false;
 	protected boolean fromApplication = false;
 	private boolean keyBackTouched = false;
 	private URI uri;
 	private ProgressDialog startingDialog;
-	
 
-    protected void setDashboardResource(int resource) {
-        dashboardResourceId = resource;
-    }
-    
-    protected void setMainWindowResource(int resource) {
-        mainWindowId = resource;
-    }
-    
-    protected void setDefaultAppName(String name) {
-        if(name == null) {
-                startApplication = false;
-        }
-        defaultAppName = name;
-    }
-	
+	protected void setDashboardResource(int resource) {
+		dashboardResourceId = resource;
+	}
+
+	protected void setMainWindowResource(int resource) {
+		mainWindowId = resource;
+	}
+
+	protected void setDefaultAppName(String name) {
+		if (name == null) {
+			startApplication = false;
+		}
+		defaultAppName = name;
+	}
+
 	protected RosAppActivity(String notificationTicker, String notificationTitle) {
 		super(notificationTicker, notificationTitle);
 	}
-	
+
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
+		if (mainWindowId == 0) {
+			Log.e("RosAndroid",
+					"You must set the dashboard resource ID in your RosAppActivity");
+			return;
+		}
+		if (dashboardResourceId == 0) {
+			Log.e("RosAndroid",
+					"You must set the dashboard resource ID in your RosAppActivity");
+			return;
+		}
 
-    	    if(mainWindowId == 0) {
-                Log.e("RosAndroid", "You must set the dashboard resource ID in your RosAppActivity");
-                return;
-    	    }
-            if(dashboardResourceId == 0) {
-                Log.e("RosAndroid", "You must set the dashboard resource ID in your RosAppActivity");
-                return;
-    	    }
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(mainWindowId);
+		robotAppName = getIntent().getStringExtra(
+				AppManager.PACKAGE + ".robot_app_name");
+		if (robotAppName == null) {
+			robotAppName = defaultAppName;
+		} else if (robotAppName.equals("AppChooser")) {
+			fromApplication = true;
+		} else {
+			fromAppChooser = true;
+			startingDialog = ProgressDialog.show(this, "Starting Robot",
+					"starting robot...", true, false);
+			startingDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+		}
 
-    	    requestWindowFeature(Window.FEATURE_NO_TITLE);
-    	    getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-    	    setContentView(mainWindowId);
-            robotAppName = getIntent().getStringExtra(AppManager.PACKAGE + ".robot_app_name");
-            if(robotAppName == null) {
-                robotAppName = defaultAppName;
-            }
-            else if(robotAppName.equals("AppChooser")){
-            	fromApplication = true;
-            }
-            else {
-            	fromAppChooser = true;
-            	 startingDialog =  ProgressDialog.show(this,
-  		               "Starting Robot", "starting robot...", true, false);
-  		    startingDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
-            }
-            
-    	    if(dashboard == null) {
-    	            dashboard = new Dashboard(this);
-    	            dashboard.setView((LinearLayout) findViewById(dashboardResourceId), new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-    	    }
+		if (dashboard == null) {
+			dashboard = new Dashboard(this);
+			dashboard.setView((LinearLayout) findViewById(dashboardResourceId),
+					new LinearLayout.LayoutParams(
+							LinearLayout.LayoutParams.WRAP_CONTENT,
+							LinearLayout.LayoutParams.WRAP_CONTENT));
+		}
 	}
-	
+
 	@Override
-	protected void init(NodeMainExecutor nodeMainExecutor){
+	protected void init(NodeMainExecutor nodeMainExecutor) {
 		this.nodeMainExecutor = nodeMainExecutor;
-		nodeConfiguration = NodeConfiguration.newPublic(
-				InetAddressFactory.newNonLoopback().getHostAddress(),
-				getMasterUri());
-		nodeMainExecutor.execute(dashboard, nodeConfiguration.setNodeName("dashboard"));
-		if(startApplication){
+		nodeConfiguration = NodeConfiguration.newPublic(InetAddressFactory
+				.newNonLoopback().getHostAddress(), getMasterUri());
+		nodeMainExecutor.execute(dashboard,
+				nodeConfiguration.setNodeName("dashboard"));
+		if (startApplication) {
 			startApp();
 		}
 	}
-	  
-	@Override
-	  public void startMasterChooser() {
-		if(!fromAppChooser && !fromApplication){
-			super.startMasterChooser();
-		}
-		else{
-			Intent intent = new Intent();
-		    intent.putExtra(AppManager.PACKAGE + ".robot_app_name", "AppChooser");
-		          try {
-		            uri = new URI(getIntent().getStringExtra("ChooserURI"));
-		          } catch (URISyntaxException e) {
-		            throw new RosRuntimeException(e);
-		          }
-			 
-	          nodeMainExecutorService.setMasterUri(uri);
-	        new AsyncTask<Void, Void, Void>() {
-	            @Override
-	            protected Void doInBackground(Void... params) {
-	              RosAppActivity.this.init(nodeMainExecutorService);
-	              return null;
-	            }
-	          }.execute();
-		}
-		
-	  }
-	
-    private void startApp() {
-        Log.i("RosAndroid", "Starting application");
-        
-        AppManager appManager = new AppManager(robotAppName);
-        appManager.setFunction("start");
 
-        
-        appManager.setStartService(new ServiceResponseListener<StartAppResponse>() {
-            @Override
-            public void onSuccess(StartAppResponse message) {
-            	if(fromAppChooser == true){
-            		startingDialog.dismiss();
-            	}
-                    Log.i("RosAndroid", "App started successfully");
- 
-            }
-            @Override
-            public void onFailure(RemoteException e) {
-                    Log.e("RosAndroid", "App failed to start!");
-            }
-        });
-        
-       nodeMainExecutor.execute(appManager, nodeConfiguration.setNodeName("start_app"));
-    }
-    
-    protected void stopApp() {
-        Log.i("RosAndroid", "Stopping application");
-        AppManager appManager = new AppManager(robotAppName);
-        appManager.setFunction("stop");
-        
-        appManager.setStopService(new ServiceResponseListener<StopAppResponse>() {
-            @Override
-            public void onSuccess(StopAppResponse message) {
-                    Log.i("RosAndroid", "App stopped successfully");
-            }
-            @Override
-            public void onFailure(RemoteException e) {
-                    Log.e("RosAndroid", "App failed to stop!");
-            }
-        });
-        
-        nodeMainExecutor.execute(appManager, nodeConfiguration.setNodeName("start_app"));
-    }
-    
-    
-    protected void releaseDashboardNode(){
-    	nodeMainExecutor.shutdownNodeMain(dashboard);
-    }
-    
+	@Override
+	public void startMasterChooser() {
+		if (!fromAppChooser && !fromApplication) {
+			super.startMasterChooser();
+		} else {
+			Intent intent = new Intent();
+			intent.putExtra(AppManager.PACKAGE + ".robot_app_name",
+					"AppChooser");
+			try {
+				uri = new URI(getIntent().getStringExtra("ChooserURI"));
+			} catch (URISyntaxException e) {
+				throw new RosRuntimeException(e);
+			}
+
+			nodeMainExecutorService.setMasterUri(uri);
+			new AsyncTask<Void, Void, Void>() {
+				@Override
+				protected Void doInBackground(Void... params) {
+					RosAppActivity.this.init(nodeMainExecutorService);
+					return null;
+				}
+			}.execute();
+		}
+
+	}
+
+	private void startApp() {
+		Log.i("RosAndroid", "Starting application");
+
+		AppManager appManager = new AppManager(robotAppName);
+		appManager.setFunction("start");
+
+		appManager
+				.setStartService(new ServiceResponseListener<StartAppResponse>() {
+					@Override
+					public void onSuccess(StartAppResponse message) {
+						if (fromAppChooser == true) {
+							startingDialog.dismiss();
+						}
+						Log.i("RosAndroid", "App started successfully");
+
+					}
+
+					@Override
+					public void onFailure(RemoteException e) {
+						Log.e("RosAndroid", "App failed to start!");
+					}
+				});
+
+		nodeMainExecutor.execute(appManager,
+				nodeConfiguration.setNodeName("start_app"));
+	}
+
+	protected void stopApp() {
+		Log.i("RosAndroid", "Stopping application");
+		AppManager appManager = new AppManager(robotAppName);
+		appManager.setFunction("stop");
+
+		appManager
+				.setStopService(new ServiceResponseListener<StopAppResponse>() {
+					@Override
+					public void onSuccess(StopAppResponse message) {
+						Log.i("RosAndroid", "App stopped successfully");
+					}
+
+					@Override
+					public void onFailure(RemoteException e) {
+						Log.e("RosAndroid", "App failed to stop!");
+					}
+				});
+		nodeMainExecutor.execute(appManager,
+				nodeConfiguration.setNodeName("start_app"));
+	}
+
+	protected void releaseDashboardNode() {
+		nodeMainExecutor.shutdownNodeMain(dashboard);
+	}
 
 	@Override
 	protected void onDestroy() {
-		if(startApplication && !keyBackTouched){
+		if (startApplication && !keyBackTouched) {
 			stopApp();
 		}
 		super.onDestroy();
 	}
 
-	
-	public boolean onKeyDown(int keyCode,KeyEvent event){
-	  if(keyCode == KeyEvent.KEYCODE_BACK && fromAppChooser){
-		keyBackTouched = true;
-		Intent intent = new Intent();
-		intent.putExtra(AppManager.PACKAGE + ".robot_app_name", "AppChooser");
-		intent.putExtra("ChooserURI", uri.toString());
-		intent.setAction("org.ros.android.android_app_chooser.AppChooser");
-		intent.addCategory("android.intent.category.DEFAULT");
-		startActivity(intent);
-		onDestroy();
-		return false;
-	  }
-	  else return super.onKeyDown(keyCode, event);
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_BACK && fromAppChooser) {
+			keyBackTouched = true;
+			Intent intent = new Intent();
+			intent.putExtra(AppManager.PACKAGE + ".robot_app_name",
+					"AppChooser");
+			intent.putExtra("ChooserURI", uri.toString());
+			intent.setAction("org.ros.android.android_app_chooser.AppChooser");
+			intent.addCategory("android.intent.category.DEFAULT");
+			startActivity(intent);
+			onDestroy();
+			return false;
+		} else
+			return super.onKeyDown(keyCode, event);
 	}
 }
