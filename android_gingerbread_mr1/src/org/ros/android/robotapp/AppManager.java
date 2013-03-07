5806ec83cfad90540943d4365933ca5498defbc2
KazutoMurase <kattyokaccho3616@gmail.com>
2013-03-06 17:44:20 -0800

    remove log message

--- android_gingerbread_mr1/src/org/ros/android/robotapp/AppManager.java
+++ android_gingerbread_mr1/src/org/ros/android/robotapp/AppManager.java
@@ -143,8 +143,6 @@ public class AppManager extends AbstractNodeMain {
 	public void listApps() {
 		String listTopic = resolver.resolve(this.listTopic).toString();
 		
-		Log.v("debugPR2",listTopic);
-
 		ServiceClient<ListAppsRequest, ListAppsResponse> listAppsClient;
 		try {
 			Log.i("RosAndroid", "List app service client created" + listTopic);
