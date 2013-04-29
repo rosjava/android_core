22301418d5113beac73beebf336d5cb6874a1078
Damon Kohler <damonkohler@google.com>
2013-04-29 15:47:32 +0200

    Adds hokuyo and polling_input_stream packages.

--- hokuyo/src/main/java/org/ros/hokuyo/scip20/TmException.java
+++ hokuyo/src/main/java/org/ros/hokuyo/scip20/TmException.java
@@ -1,12 +1,12 @@
 /*
  * Copyright (C) 2011 Google Inc.
- *
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -14,8 +14,25 @@
  * the License.
  */
 
-include 'android_gingerbread_mr1', 'android_tutorial_pubsub', 'android_honeycomb_mr2',
-        'android_tutorial_teleop', 'android_tutorial_hokuyo', 'android_acm_serial',
-        'android_tutorial_camera', 'android_tutorial_image_transport',
-        'android_tutorial_map_viewer', 'android_benchmarks', 'docs'
+package org.ros.hokuyo.scip20;
 
+/**
+ * @author damonkohler@google.com (Damon Kohler)
+ */
+public class TmException extends RuntimeException {
+
+  public TmException(String status) {
+    super(getMessage(status));
+  }
+
+  private static String getMessage(String status) {
+    if (status.equals("01")) {
+      return "Invalid control code.";
+    }
+    if (status.equals("04")) {
+      return "Adjust mode is off when requested for time.";
+    }
+
+    return "Unknown status code: " + status;
+  }
+}
