f4f4c5b56f5d1e86ab1bfd7e44cc8c7493dec689
dwlee <dwlee@yujinrobot.com>
2015-03-26 13:08:19 +0900

    update essential function which is addLayer and delete null check in onCreate

--- android_15/src/org/ros/android/view/visualization/VisualizationView.java
+++ android_15/src/org/ros/android/view/visualization/VisualizationView.java
@@ -47,167 +47,143 @@ import java.util.List;
  */
 public class VisualizationView extends GLSurfaceView implements NodeMain {
 
-  private static final boolean DEBUG = false;
+    private static final boolean DEBUG = false;
 
-  private final Object mutex = new Object();
-  private final FrameTransformTree frameTransformTree = new FrameTransformTree();
-  private final XYOrthographicCamera camera = new XYOrthographicCamera(frameTransformTree);
+    private final Object mutex = new Object();
+    private final FrameTransformTree frameTransformTree = new FrameTransformTree();
+    private final XYOrthographicCamera camera = new XYOrthographicCamera(frameTransformTree);
 
-  private List<Layer> layers;
-  private XYOrthographicRenderer renderer;
-  private ConnectedNode connectedNode;
+    private List<Layer> layers;
+    private XYOrthographicRenderer renderer;
+    private ConnectedNode connectedNode;
 
-  public VisualizationView(Context context) {
-    super(context);
-  }
-
-  public VisualizationView(Context context, AttributeSet attrs) {
-    super(context, attrs);
-  }
-
-
-
-  /**
-   * Must be called in {@link Activity#onCreate(Bundle)}.
-   * If you want to render layer, you must call addLayer before startLayer
-   * dwlee 2015.3.18
-   * @void
-   */
-  public void onCreate() {
-      setDebugFlags(DEBUG_CHECK_GL_ERROR);
-      if (DEBUG) {
-          // Turn on OpenGL logging.
-          setDebugFlags(getDebugFlags() | DEBUG_LOG_GL_CALLS);
-      }
-      setEGLConfigChooser(8, 8, 8, 8, 0, 0);
-      getHolder().setFormat(PixelFormat.TRANSLUCENT);
-      renderer = new XYOrthographicRenderer(this);
-      setRenderer(renderer);
-  }
-  public void addLayer(Layer layer) {
-      layers.add(layer);
-  }
-
-  public void removeLayer(Layer layer) {
-      layer.onShutdown(this, connectedNode);
-      layers.remove(layer);
-  }
-
-  /**
-   * Must be called in {@link Activity#onCreate(Bundle)}.
-   *
-   * @param layers
-   */
-  public void onCreate(List<Layer> layers) {
-    Preconditions.checkNotNull(layers);
-    this.layers = layers;
-    setDebugFlags(DEBUG_CHECK_GL_ERROR);
-    if (DEBUG) {
-      // Turn on OpenGL logging.
-      setDebugFlags(getDebugFlags() | DEBUG_LOG_GL_CALLS);
+    public VisualizationView(Context context) {
+        super(context);
     }
-    setEGLConfigChooser(8, 8, 8, 8, 0, 0);
-    getHolder().setFormat(PixelFormat.TRANSLUCENT);
-    renderer = new XYOrthographicRenderer(this);
-    setRenderer(renderer);
-  }
 
-  /**
-   * Must be called in {@link RosActivity#init(NodeMainExecutor)}
-   *
-   * @param nodeMainExecutor
-   */
-  public void init(NodeMainExecutor nodeMainExecutor) {
-    Preconditions.checkNotNull(layers);
-    for (Layer layer : layers) {
-      layer.init(nodeMainExecutor);
+    public VisualizationView(Context context, AttributeSet attrs) {
+        super(context, attrs);
     }
-  }
 
-  @Override
-  public GraphName getDefaultNodeName() {
-    return GraphName.of("android_15/visualization_view");
-  }
-
-  @Override
-  public boolean onTouchEvent(MotionEvent event) {
-    for (Layer layer : Lists.reverse(layers)) {
-      if (layer.onTouchEvent(this, event)) {
-        return true;
-      }
-    }
-    return super.onTouchEvent(event);
-  }
-
-  public XYOrthographicRenderer getRenderer() {
-    return renderer;
-  }
-
-  public XYOrthographicCamera getCamera() {
-    return camera;
-  }
-
-  public FrameTransformTree getFrameTransformTree() {
-    return frameTransformTree;
-  }
-
-  public List<Layer> getLayers() {
-    return Collections.unmodifiableList(layers);
-  }
-
-  @Override
-  public void onStart(ConnectedNode connectedNode) {
-    this.connectedNode = connectedNode;
-    startTransformListener();
-    startLayers();
-  }
-
-  private void startTransformListener() {
-    final Subscriber<tf2_msgs.TFMessage> tfSubscriber =
-        connectedNode.newSubscriber("tf", tf2_msgs.TFMessage._TYPE);
-    tfSubscriber.addMessageListener(new MessageListener<tf2_msgs.TFMessage>() {
-      @Override
-      public void onNewMessage(tf2_msgs.TFMessage message) {
-        synchronized (mutex) {
-          for (geometry_msgs.TransformStamped transform : message.getTransforms()) {
-            frameTransformTree.update(transform);
-          }
+    /**
+     * Must be called in {@link Activity#onCreate(Bundle)}.
+     *
+     * @param layers
+     */
+    public void onCreate(List<Layer> layers) {
+        Preconditions.checkNotNull(layers);
+        this.layers = layers;
+        setDebugFlags(DEBUG_CHECK_GL_ERROR);
+        if (DEBUG) {
+            // Turn on OpenGL logging.
+            setDebugFlags(getDebugFlags() | DEBUG_LOG_GL_CALLS);
         }
-      }
-    });
-    final Subscriber<tf2_msgs.TFMessage> tfStaticSubscriber =
-        connectedNode.newSubscriber("tf_static", tf2_msgs.TFMessage._TYPE);
-    tfStaticSubscriber.addMessageListener(new MessageListener<tf2_msgs.TFMessage>() {
-      @Override
-      public void onNewMessage(tf2_msgs.TFMessage message) {
-        synchronized (mutex) {
-          for (geometry_msgs.TransformStamped transform : message.getTransforms()) {
-            frameTransformTree.update(transform);
-          }
+        setEGLConfigChooser(8, 8, 8, 8, 0, 0);
+        getHolder().setFormat(PixelFormat.TRANSLUCENT);
+        renderer = new XYOrthographicRenderer(this);
+        setRenderer(renderer);
+    }
+
+    /**
+     * Must be called in {@link RosActivity#init(NodeMainExecutor)}
+     *
+     * @param nodeMainExecutor
+     */
+    public void init(NodeMainExecutor nodeMainExecutor) {
+        Preconditions.checkNotNull(layers);
+        for (Layer layer : layers) {
+            layer.init(nodeMainExecutor);
         }
-      }
-    });
-  }
-
-  private void startLayers() {
-    for (Layer layer : layers) {
-      layer.onStart(this, connectedNode);
     }
-  }
 
-  @Override
-  public void onShutdown(Node node) {
-    for (Layer layer : layers) {
-      layer.onShutdown(this, node);
+    @Override
+    public GraphName getDefaultNodeName() {
+        return GraphName.of("android_15/visualization_view");
     }
-    this.connectedNode = null;
-  }
 
-  @Override
-  public void onShutdownComplete(Node node) {
-  }
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        for (Layer layer : Lists.reverse(layers)) {
+            if (layer.onTouchEvent(this, event)) {
+                return true;
+            }
+        }
+        return super.onTouchEvent(event);
+    }
 
-  @Override
-  public void onError(Node node, Throwable throwable) {
-  }
+    public XYOrthographicRenderer getRenderer() {
+        return renderer;
+    }
+
+    public XYOrthographicCamera getCamera() {
+        return camera;
+    }
+
+    public FrameTransformTree getFrameTransformTree() {
+        return frameTransformTree;
+    }
+
+    public List<Layer> getLayers() {
+        return Collections.unmodifiableList(layers);
+    }
+
+    @Override
+    public void onStart(ConnectedNode connectedNode) {
+        this.connectedNode = connectedNode;
+        startTransformListener();
+        startLayers();
+    }
+
+    private void startTransformListener() {
+        final Subscriber<tf2_msgs.TFMessage> tfSubscriber =
+                connectedNode.newSubscriber("tf", tf2_msgs.TFMessage._TYPE);
+        tfSubscriber.addMessageListener(new MessageListener<tf2_msgs.TFMessage>() {
+            @Override
+            public void onNewMessage(tf2_msgs.TFMessage message) {
+                synchronized (mutex) {
+                    for (geometry_msgs.TransformStamped transform : message.getTransforms()) {
+                        frameTransformTree.update(transform);
+                    }
+                }
+            }
+        });
+        final Subscriber<tf2_msgs.TFMessage> tfStaticSubscriber =
+                connectedNode.newSubscriber("tf_static", tf2_msgs.TFMessage._TYPE);
+        tfStaticSubscriber.addMessageListener(new MessageListener<tf2_msgs.TFMessage>() {
+            @Override
+            public void onNewMessage(tf2_msgs.TFMessage message) {
+                synchronized (mutex) {
+                    for (geometry_msgs.TransformStamped transform : message.getTransforms()) {
+                        frameTransformTree.update(transform);
+                    }
+                }
+            }
+        });
+    }
+
+    private void startLayers() {
+        for (Layer layer : layers) {
+            layer.onStart(this, connectedNode);
+        }
+    }
+
+    public void addLayer(Layer layer) {
+        layers.add(layer);
+    }
+
+    @Override
+    public void onShutdown(Node node) {
+        for (Layer layer : layers) {
+            layer.onShutdown(this, node);
+        }
+        this.connectedNode = null;
+    }
+
+    @Override
+    public void onShutdownComplete(Node node) {
+    }
+
+    @Override
+    public void onError(Node node, Throwable throwable) {
+    }
 }
