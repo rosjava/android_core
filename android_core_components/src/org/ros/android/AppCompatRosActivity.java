0ce768238336d537f48d3a38dcfd2e56145af52f
Patrick Roncagliolo <ronca.pat@gmail.com>
2019-03-09 13:37:37 +0100

    More linting rollback

--- android_core_components/src/org/ros/android/AppCompatRosActivity.java
+++ android_core_components/src/org/ros/android/AppCompatRosActivity.java
@@ -18,93 +18,93 @@ import java.net.URI;
  * to be used with AppCompat.
  */
 public abstract class AppCompatRosActivity extends RosActivity {
-  private AppCompatDelegate mDelegate;
+    private AppCompatDelegate mDelegate;
 
-  public AppCompatRosActivity() {
-    super("", "");
-  }
-
-  protected AppCompatRosActivity(String notificationTicker, String notificationTitle) {
-    super(notificationTicker, notificationTitle);
-  }
-
-  protected AppCompatRosActivity(String notificationTicker, String notificationTitle, URI customMasterUri) {
-    super(notificationTicker, notificationTitle, customMasterUri);
-  }
-
-  protected AppCompatRosActivity(String notificationTicker, String notificationTitle, Class<?> activity, int requestCode) {
-    super(notificationTicker, notificationTitle, activity, requestCode);
-  }
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    getDelegate().installViewFactory();
-    getDelegate().onCreate(savedInstanceState);
-    super.onCreate(savedInstanceState);
-  }
-  @Override
-  protected void onPostCreate(Bundle savedInstanceState) {
-    super.onPostCreate(savedInstanceState);
-    getDelegate().onPostCreate(savedInstanceState);
-  }
-  public ActionBar getSupportActionBar() {
-    return getDelegate().getSupportActionBar();
-  }
-  public void setSupportActionBar(@Nullable Toolbar toolbar) {
-    getDelegate().setSupportActionBar(toolbar);
-  }
-  @Override
-  public MenuInflater getMenuInflater() {
-    return getDelegate().getMenuInflater();
-  }
-  @Override
-  public void setContentView(@LayoutRes int layoutResID) {
-    getDelegate().setContentView(layoutResID);
-  }
-  @Override
-  public void setContentView(View view) {
-    getDelegate().setContentView(view);
-  }
-  @Override
-  public void setContentView(View view, ViewGroup.LayoutParams params) {
-    getDelegate().setContentView(view, params);
-  }
-  @Override
-  public void addContentView(View view, ViewGroup.LayoutParams params) {
-    getDelegate().addContentView(view, params);
-  }
-  @Override
-  protected void onPostResume() {
-    super.onPostResume();
-    getDelegate().onPostResume();
-  }
-  @Override
-  protected void onTitleChanged(CharSequence title, int color) {
-    super.onTitleChanged(title, color);
-    getDelegate().setTitle(title);
-  }
-  @Override
-  public void onConfigurationChanged(Configuration newConfig) {
-    super.onConfigurationChanged(newConfig);
-    getDelegate().onConfigurationChanged(newConfig);
-  }
-  @Override
-  protected void onStop() {
-    super.onStop();
-    getDelegate().onStop();
-  }
-  @Override
-  protected void onDestroy() {
-    super.onDestroy();
-    getDelegate().onDestroy();
-  }
-  public void invalidateOptionsMenu() {
-    getDelegate().invalidateOptionsMenu();
-  }
-  private AppCompatDelegate getDelegate() {
-    if (mDelegate == null) {
-      mDelegate = AppCompatDelegate.create(this, null);
+    public AppCompatRosActivity() {
+        super("", "");
     }
-    return mDelegate;
-  }
+
+    protected AppCompatRosActivity(String notificationTicker, String notificationTitle) {
+        super(notificationTicker, notificationTitle);
+    }
+
+    protected AppCompatRosActivity(String notificationTicker, String notificationTitle, URI customMasterUri) {
+        super(notificationTicker, notificationTitle, customMasterUri);
+    }
+
+    protected AppCompatRosActivity(String notificationTicker, String notificationTitle, Class<?> activity, int requestCode) {
+        super(notificationTicker, notificationTitle, activity, requestCode);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        getDelegate().installViewFactory();
+        getDelegate().onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);
+    }
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        getDelegate().onPostCreate(savedInstanceState);
+    }
+    public ActionBar getSupportActionBar() {
+        return getDelegate().getSupportActionBar();
+    }
+    public void setSupportActionBar(@Nullable Toolbar toolbar) {
+        getDelegate().setSupportActionBar(toolbar);
+    }
+    @Override
+    public MenuInflater getMenuInflater() {
+        return getDelegate().getMenuInflater();
+    }
+    @Override
+    public void setContentView(@LayoutRes int layoutResID) {
+        getDelegate().setContentView(layoutResID);
+    }
+    @Override
+    public void setContentView(View view) {
+        getDelegate().setContentView(view);
+    }
+    @Override
+    public void setContentView(View view, ViewGroup.LayoutParams params) {
+        getDelegate().setContentView(view, params);
+    }
+    @Override
+    public void addContentView(View view, ViewGroup.LayoutParams params) {
+        getDelegate().addContentView(view, params);
+    }
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        getDelegate().onPostResume();
+    }
+    @Override
+    protected void onTitleChanged(CharSequence title, int color) {
+        super.onTitleChanged(title, color);
+        getDelegate().setTitle(title);
+    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        getDelegate().onConfigurationChanged(newConfig);
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        getDelegate().onStop();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        getDelegate().onDestroy();
+    }
+    public void invalidateOptionsMenu() {
+        getDelegate().invalidateOptionsMenu();
+    }
+    private AppCompatDelegate getDelegate() {
+        if (mDelegate == null) {
+            mDelegate = AppCompatDelegate.create(this, null);
+        }
+        return mDelegate;
+    }
 }
